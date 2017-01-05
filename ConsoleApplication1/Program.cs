3ab620d14c37a6317111503a53c5c29a8e8e2e0b
sergshloyda <shloyders@gmail.com>
2017-01-05 21:05:41 +0300

    Перевод на русский

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Привет мир!!!");
             Console.ReadLine();
             Console.ReadLine();
         }
